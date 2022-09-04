398859cfd6ba17b0861f8c43ff3a15fb413d1972
James Reinhold <mrjamesreinhold@gmail.com>
2022-09-04 13:02:16 +0200

    started budget app

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "family_budget.users",
-    # Your stuff: custom apps go here
+    "budget"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
